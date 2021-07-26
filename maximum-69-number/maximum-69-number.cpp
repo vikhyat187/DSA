622dd3db61a415e83b438f9b45f1e88d3010ae46
vikhyat187 <52795644+vikhyat187@users.noreply.github.com>
2021-07-26 23:38:50 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (97.57%) - LeetHub

--- maximum-69-number/maximum-69-number.cpp
+++ maximum-69-number/maximum-69-number.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
     int maximum69Number (int num) {
-        string res= to_string(num);
-        for (auto &ch : res){
-            if (ch == '6'){
-                ch= '9';
-                break;
-                
+        long long int pos6=1,maxPos6=0;
+        int n=num;
+        while(n>0){
+            int digit = n%10;
+            if (digit ==6 ){
+                maxPos6= pos6;//just find the left most 6 and then to convert suppose 699 to 999
+                // we add 300 3*maxPos6 that is the place value ones,tens,hundreds...
             }
+            pos6*=10;
+            n/=10;
         }
-        return stoi(res);
+        return num+ 3*maxPos6;
     }
 };
