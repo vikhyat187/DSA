b9169cd645e4c4c2338446be346d90a3fe5fcb66
vikhyat187 <52795644+vikhyat187@users.noreply.github.com>
2021-08-07 01:13:06 +0530

    Time: 20 ms (51.59%), Space: 19.5 MB (86.75%) - LeetHub

---
+++ majority-element/majority-element.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int,int>map;
+        for (int i=0;i<nums.size();i++){
+            map[nums[i]]++;
+        }
+        for (int i=0;i<nums.size();i++){
+            if (map[nums[i]] > floor(nums.size()/2))return nums[i];
+        }
+        return -1;
+    }
+};
