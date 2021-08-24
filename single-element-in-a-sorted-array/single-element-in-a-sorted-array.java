66ce574321649c047e4c7bde205c02e5766d2330
vikhyat187 <52795644+vikhyat187@users.noreply.github.com>
2021-08-24 22:28:35 +0530

    Time: 0 ms (100.00%), Space: 38.9 MB (76.55%) - LeetHub

--- single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -1,9 +1,15 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        int res=0;
-        for (int i=0;i<nums.length;i++){
-            res=res^nums[i];
+        int low =0,high= nums.length-1;
+        if (nums.length==1)return nums[0];
+        while(low<high){
+            int mid = low + (high-low)/2;
+            if (mid %2==0 && nums[mid+1] == nums[mid] || (mid%2==1 && nums[mid] == nums[mid-1])){
+                //the single ele is on the right
+                low = mid+1;
+            }
+            else high= mid;
         }
-        return res;
+        return nums[low];
     }
 }
