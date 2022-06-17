afd9918d815007439654df0458b1af4ce5454609
Vikhyat Bhatnagar <52795644+vikhyat187@users.noreply.github.com>
2022-06-18 01:56:21 +0530

    Time: 25 ms (18.63%), Space: 48.6 MB (13.34%) - LeetHub

---
+++ 36-valid-sudoku/36-valid-sudoku.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        Set<String> seen = new HashSet<>();
+        for (int i = 0; i < 9; i++){
+            for (int j = 0; j < 9; j++){
+                char number = board[i][j];
+                if(number == '.'){
+                    continue;
+                }
+                else if (seen.contains(number +"_R_"+ i) ||
+                         seen.contains(number +"_C_"+ j) ||
+                         seen.contains(number +"_B_"+ i/3 +"_"+ j/3))
+                         {
+                    
+                             return false;
+                         }
+                         seen.add(number + "_R_"+ i);
+                         seen.add(number + "_C_"+ j);
+                         seen.add(number + "_B_"+ i/3 + "_" + j/3);
+                }
+            }
+        return true;
+    }
+}
