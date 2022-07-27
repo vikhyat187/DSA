4603b04050247378c9546e32094a565fb90c350c
Vikhyat Bhatnagar <52795644+vikhyat187@users.noreply.github.com>
2022-07-27 20:37:11 +0530

    Time: 1 ms (100.00%), Space: 73.6 MB (59.34%) - LeetHub

---
+++ 53-maximum-subarray/53-maximum-subarray.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        
+        
+        int currSum =0 , maxSum  = nums[0];
+        
+        for (int i =0; i < nums.length; i++){
+            currSum += nums[i];
+            if(currSum > maxSum){
+                maxSum = currSum;
+            }
+            if(currSum < 0){
+                currSum = 0;
+            }
+        }
+        return maxSum;
+    }
+}
